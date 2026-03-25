793f42a7d033dffeb9848688886d8224d10de1fd
trinath06183 <trinathnayak06183@gmail.com>
2026-03-25 23:51:35 +0530

    remode duplicate homepage.html, update the urls.py and views.py for login page anf free trail page, with updating the buttons with linked to those pages

--- IronJaw/urls.py
+++ IronJaw/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage_view, name='homepage'),
     path('login/', views.login_view, name='login'),
+    path('start_free/', views.start_free_view, name='start_free'),
 ]
--- IronJaw/views.py
+++ IronJaw/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def homepage_view(request):
-    return render(request, 'homepage.html')
+    return render(request, 'frontpage.html')
 
 def login_view(request):
-    return render(request, 'login.html')
+    return render(request, 'login_page.html')
+
+def start_free_view(request):
+    return render(request, 'start_free.html')
